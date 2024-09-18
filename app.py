4302e92c039ee1c027b85b212cf00c07056c1bb7
TytanGorilla <65769501+TytanGorilla@users.noreply.github.com>
2024-09-18 14:55:24 +0200

    Added: login & registration templates & Flask-Session

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
 
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html")
     #return "TYTANNNN"
+
+if __name__ == '__main__':
+    app.run(debug=True)
